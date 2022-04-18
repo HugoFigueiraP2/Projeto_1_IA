678d3003f703ecad906cdbe0cdfa918750e56ed9
HugoFigueiraP2 <hugofigueira.dev@gmail.com>
2022-04-18 23:07:11 +0100

    Adding limiter to spawn AI Agents

--- Projeto_1_IA/Assets/Scripts/Spawner.cs
+++ Projeto_1_IA/Assets/Scripts/Spawner.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public GameObject AI_Agent;
-    float timer = 0.0f;
+    public GameObject parent;
+    public int numberToSpawn;
+    public int limit = 20;
+    public float timer = 1.0f;
+
+    float spawnTimer;
     
     // Start is called before the first frame update
     void Start()
     {
-        timer = Time.time;
+        spawnTimer = timer;
     }
 
     // Update is called once per frame
     void Update()
-    {   //while (timer <= 200)
-        //{
-            if (Time.time - timer > 1)
+    {   
+        if (parent.transform.childCount < limit)
+        {
+            spawnTimer -= Time.deltaTime;
+            if (spawnTimer <= 0f)
             {
-                Instantiate(AI_Agent, transform.position, Quaternion.identity);
-                timer = Time.time;
+                for (int i = 0; i < numberToSpawn; i++)
+                {
+                    Instantiate(AI_Agent, new Vector3(this.transform.position.x + GetModifier(),
+                        this.transform.position.y + GetModifier()), Quaternion.identity, parent.transform);
+                }
+                spawnTimer = timer;
             }
-        //}
+        }
         
+    }
+
+    float GetModifier()
+    {
+        float modifier = Random.Range(0f, 1f);
+        if (Random.Range(0, 2) > 0)
+            return -modifier;
+        else
+            return modifier;
     }
 }
